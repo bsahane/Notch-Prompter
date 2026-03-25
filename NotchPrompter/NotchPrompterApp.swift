6f6529db8d1f780753233a07f131f24b11956a34
Bhushan Sahane <bsahane@redhat.com>
2026-03-25 20:47:17 +0530

    Initial Commit

---
+++ NotchPrompter/NotchPrompterApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchPrompterApp.swift
+//  NotchPrompter
+//
+//  Created by Bhushan Sahane on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchPrompterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
